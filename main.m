32ba4b7090d0356b3159aba2004917b991d1500b
kyoungminlee1023 <lkm001023@skku.edu>
2025-06-01 20:27:29 +0900

    결과저장 방식 확인해주세요(trial 3변경함)

--- main.m
+++ main.m
@@ -1,12 +1,12 @@
 function main()
-    Screen('Preference','SkipSyncTests', 1);
+    Screen('Preference','SkipSyncTests', 2);
     [windowPtr, rect] = Screen('OpenWindow', 0, [255 255 255], [0 0 2560 1600]);
     InitializePsychSound(1);
-
+%
     %% 설문조사 실행
     responses = runSurvey(windowPtr, rect);
     WaitSecs(2);
-    Screen('FillRect', windowPtr, [150 150 150], [0 0 2560 1600]);
+    Screen('FillRect', windowPtr, [150 150 150],[0 0 2560 1600]);
     Screen('Flip', windowPtr);
 
     %% 카드 조건 불러오기
@@ -19,12 +19,13 @@ function main()
     [xGrid, yGrid] = meshgrid(300:450:1650, 200:330:860);
     positions = [xGrid(:), yGrid(:)];
     
-    %결과 입력 생성
-    RTs = zeros(1, 10);
-    errors = zeros(1, 10);
+    %실험 초기화
+    RTs = zeros(1, 3);
+    errors = zeros(1, 3);
+    trialData(3) = struct();
 
     % 모든 문제 반복
-    for trialIdx = 1:10
+    for trialIdx = 1:3
         card_set = problems(trialIdx).cards;
         for j = 1:12
             card = card_set(j);
@@ -68,15 +69,31 @@ function main()
         RTs(trialIdx) = EndTime - StartTime;
         error(trialIdx) = error;
         disp(['Trial ', num2str(trialIdx), ' - RT: ', num2str(RTs(trialIdx)), ', Error: ', num2str(error)]);
+        
+        trialData(trialIdx).trial_index = trialIdx;
+        trialData(trialIdx).correct_indices = correctCardIndices;
+        trialData(trialIdx).response_time = RTs;
+        trialData(trialIdx).error = error;
+        trialData(trialIdx).card_order = shuffledCards;  % 카드 정보 저장
 
         % 키 누를 때까지 대기 후 다음 trial
         DrawFormattedText(windowPtr, 'Press any key for next trial', 'center', 'center', [0 0 0]);
         Screen('Flip', windowPtr);
         KbStrokeWait;
     end
+        %% 실험 결과 저장
+       participant_id = ['P' datestr(now, 'yyyymmdd_HHMMSS')];
+       filename = [responses.subNum '_' participant_id '_results.mat'];
 
-    sca;
+       results = struct();
+       results.participant_id = participant_id;
+       results.responses = responses;
+       results.RTs = RTs;
+       results.errors = errors;
+       results.trials = trialData;
 
-    %% 결과 저장
-    save('final_experiment_results.mat', 'responses', 'RTs', 'error');
+       save(filename, 'results');
+    
+       sca;
+
 end
